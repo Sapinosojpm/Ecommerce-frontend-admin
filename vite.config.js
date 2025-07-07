fdd4820d9816a3a28f5efbcb04d438e8acfde735
John Paul Mille <sapinosojohnpaulmille@gmail.com>
2025-07-07 15:36:38 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: '/',
   plugins: [react()],
-  server: { port: 5174, proxy: { '/api': 'http://localhost:4000' } },
+  server: { port: 5174, proxy: { '/api': 'https://ecommerce-server-d8a1.onrender.com' } },
   resolve: {
     alias: {
       // Add an alias to resolve "@deck.gl/widgets" if necessary
